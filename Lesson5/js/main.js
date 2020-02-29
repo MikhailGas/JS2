d70cfa2581cb1ec9531a09e5b822d4568589dc2d
Mihail Gasanov <m-gasanov@ya.ru>
2020-02-29 15:59:53 +0500

    Домашнее задание к уроку №5

--- Lesson5/js/main.js
+++ Lesson5/js/main.js
@@ -1,11 +1,21 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-onst app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
     catalogUrl: '/catalogData.json',
     products: [],
-    imgCatalog: 'https://placehold.it/200x150'
+    searchProducts: [],
+    isSearch: false,
+    goods: [],
+    imgCatalog: 'https://placehold.it',
+    searchLine: '',
+    isVisibleCart: false,
+    cartList: [],
+    noGoods: false,
+    totalPrice: 0,
+    goodsCount: 0,
+    showCount: false,
   },
   methods: {
     getJson(url){
@@ -15,10 +25,80 @@ onst app = new Vue({
           console.log(error);
         })
     },
-    addProduct(product){
-      console.log(product.id_product);
-    }
+    addToCart(id){
+      this.goodsCount++;
+      this.showCount = true;
+      let flag = true;
+      this.cartList.forEach(elem => {
+          if(elem.id_product == id) {
+              elem.product_quantity++;
+              flag = false;
+          }
+      })
+      if(flag){
+          this.products.forEach(elem => {
+              if(elem.id_product == id){
+                  this.cartList.push({
+                      id_product: elem.id_product,
+                      product_name: elem.product_name,
+                      price: elem.price,
+                      product_quantity: 1,
+                  });
+                  
+
+              }
+              //break;
+          })
+          flag = !flag;
+          this.noGoods = false;
+          
+      }
+      this.totalPriceCart();
+    },
+      
+    delProduct(id) {
+        this.goodsCount--;
+        let flag = true;
+      this.cartList.forEach(elem => {
+          if(elem.id_product == id && elem.product_quantity > 1) {
+              elem.product_quantity--;
+              flag = false;
+          }
+      })
+      if(flag){
+          this.cartList = this.cartList.filter(elem => elem.id_product != id);
+          flag = !flag;
+          this.noGoods = false;
+          
+      }
+      this.totalPriceCart();
+      if(this.cartList.length == 0) {
+          this.noGoods = true;
+          this.showCount = false;
+      }
+    },
+      
+    FilterGoods(event){
+        event.preventDefault();
+        if(this.searchLine){
+            reg = new RegExp(`${this.searchLine}`, 'ig');
+            this.searchProducts = this.products.filter(data => reg.test(data.product_name));  
+            this.goods = this.searchProducts;
+        } else this.goods = this.products;
+    },
+    cartListShow(){
+        this.isVisibleCart = !this.isVisibleCart
+        if(this.cartList.length == 0) this.noGoods = true;
+        else this.noGoods = false;
+    },
+     totalPriceCart(){
+         this.totalPrice = 0;
+         this.cartList.forEach(elem => {
+             this.totalPrice += elem.price * elem.product_quantity;
+         })
+     }
   },
+    
   // хук жизненного цикла
   mounted(){
     this.getJson(`${API + this.catalogUrl}`)
@@ -26,6 +106,9 @@ onst app = new Vue({
         for(let el of data){
           this.products.push(el);
         }
+        this.goods = this.products;
+        this.goods.push({product_name:'Лампа'});
       });
   }
 });
+
